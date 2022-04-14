7af2cc15597d677ca0999f9406c3ec873c3937be
William Firmino <williamwf07@gmail.com>
2022-04-14 18:31:41 -0300

    Creation of the default, error and disabled input

---
+++ src/components/input/input.jsx
@@ -0,0 +1,53 @@
+import styles from "./styles.module.scss";
+
+export function Input(props) {
+  return (
+    <div
+      className={`
+      ${styles.inputDiv}
+      ${props.states ? styles[props.states] : ""}
+      ${props.error ? styles.errorLabel : ""}
+      `}
+    >
+      <label className={styles.label} for="name">
+        Label
+      </label>
+
+      <input
+        className={`
+        ${styles.input}
+        ${props.states ? styles[props.states] : ""}
+        ${props.error ? styles.error : ""}
+        `}
+        type="text"
+        id="name"
+        name="name"
+        required
+        placeholder="Placeholder"
+        disabled={props.disabled ? "disabled" : ""}
+      ></input>
+    </div>
+    // <button
+    //   className={`
+    //   ${styles.button}
+    //   ${styles[props.variant]}
+    //   ${styles[props.states]}
+    //   ${props.disableShadow ? styles.disableShadow : undefined}
+    //   ${styles[props.size]}
+    //   ${styles[props.color]}`}
+    //   disabled={props.disabled ? "disabled" : ""}
+    // >
+    //   {props.startIcon ? (
+    //     <span className={`material-icons ${styles.materialIcons}`}>
+    //       {props.startIcon}
+    //     </span>
+    //   ) : undefined}
+    //   {props.children ? props.children : "Default"}
+    //   {props.endIcon ? (
+    //     <span className={`material-icons ${styles.materialIcons}`}>
+    //       {props.endIcon}
+    //     </span>
+    //   ) : undefined}
+    // </button>
+  );
+}
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,9 +1,46 @@
+import { Input } from "../input/input";
 import styles from "./styles.module.scss";
 
 export function Main() {
   return (
     <main className={styles.main}>
       <h1 className={styles.h1}>Inputs</h1>
+      <div className={styles.inputBox}>
+        <div>
+          <p>&lt;Input /&gt;</p>
+          <Input />
+        </div>
+        <div>
+          <p className={styles.states}>&amp;:hover</p>
+          <Input states="hoverActive" />
+        </div>
+        <div>
+          <p className={styles.states}>&amp;:focus</p>
+          <Input states="focusActive" />
+        </div>
+      </div>
+
+      <div className={styles.inputBox}>
+        <div>
+          <p>&lt;Input error /&gt;</p>
+          <Input error />
+        </div>
+        <div>
+          <p className={styles.states}>&amp;:hover</p>
+          <Input error states="errorHoverActive" />
+        </div>
+        <div>
+          <p className={styles.states}>&amp;:focus</p>
+          <Input error states="errorFocusActive" />
+        </div>
+      </div>
+
+      <div className={styles.inputBox}>
+        <div>
+          <p>&lt;Input disabled /&gt;</p>
+          <Input disabled />
+        </div>
+      </div>
     </main>
   );
 }
